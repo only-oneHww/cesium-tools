959c875b87ebe15670e05e511ded94aacdfa04a0
jiangpen.liu <2279830564@qq.com>
2022-08-07 01:24:11 +0800

    初始化项目

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import '/Widgets/widgets.css'
+import '/Widgets/widgets.css'
+
+
+App.use(ElementPlus)
+createApp(App).use(store).use(router).mount('#app')
