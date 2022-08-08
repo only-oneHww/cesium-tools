f56674a368145cd7ae296aaf48bb3daf0e6304a6
jiangpen.liu <2279830564@qq.com>
2022-08-08 10:18:30 +0800

    修改main.js引入

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,5 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '/Widgets/widgets.css'
-import '/Widgets/widgets.css'
 
-
-App.use(ElementPlus)
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(ElementPlus).use(router).mount('#app')
